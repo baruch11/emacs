a357230f8b09efe79316137b68ba79283299b404
CHARLES-HENRI PRAT <charprat@yahoo.fr>
2023-06-07 15:10:42 +0200

    activate conda env init in eshell

--- init.el
+++ init.el
@@ -101,7 +101,7 @@
   ;; if you want interactive shell support, include:
   ;;(conda-env-initialize-interactive-shells)
   ;; if you want eshell support, include:
-  ;;(conda-env-initialize-eshell)
+  (conda-env-initialize-eshell)
   ;; if you want auto-activation (see below for details), include:
   ;;(conda-env-autoactivate-mode t)
   (conda-env-activate "base")
