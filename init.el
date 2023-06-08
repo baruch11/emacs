0becbcc2007abdd9c56a42f6938ea825e95eb193
CHARLES-HENRI PRAT <charprat@yahoo.fr>
2023-06-08 10:29:12 +0200

    evil mode with crafted-evil (default disabled)

--- init.el
+++ init.el
@@ -197,6 +197,7 @@
 (set-face-attribute 'default nil :height 140)
 
 
+
 ;; PYTHONPATH to curdir
 (defun my/set-pythonpath_curdir()
   "SET PYTHONPATH TO CURRENT DIR."
@@ -294,42 +295,8 @@ apps are not started from a shell."
 (add-to-list 'Info-default-directory-list
              "~/.emacs.d/info/files")
 
-(defun rune/evil-hook ()
-  (dolist (mode '(custom-mode
-                  eshell-mode
-                  git-rebase-mode
-                  erc-mode
-		  Buffer-menu-mode
-                  circe-server-mode
-                  circe-chat-mode
-                  circe-query-mode
-                  sauron-mode
-                  term-mode))
-   (add-to-list 'evil-emacs-state-modes mode)))
-
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  ;;(setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
-  :hook (evil-mode . rune/evil-hook)
-  :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
-
 
 (load-file "~/.emacs.d/roam-tune.el")
-
-
 
 (use-package swiper)
 
