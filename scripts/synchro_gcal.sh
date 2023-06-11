e777554f1a50342b56444adee7615702382007b8
CHARLES-HENRI PRAT <charprat@yahoo.fr>
2023-06-11 21:37:43 +0200

    add env variable for google cal address

--- scripts/synchro_gcal.sh
+++ scripts/synchro_gcal.sh
@@ -5,9 +5,7 @@ WGET=/usr/local/bin/wget
 ICS2ORG=/Users/charlesprat/.local/bin/ical2org.awk
 ICSFILE=/Users/charlesprat/.emacs.d/bouboulinos.ics
 ORGFILE=/Users/charlesprat/.emacs.d/bouboulinos.org
-URL=my private google cal address
-
 # no customization needed below
 
-$WGET -O $ICSFILE $URL
+$WGET -O $ICSFILE $GCAL_PRIVATE_ADDRESS
 $ICS2ORG < $ICSFILE > $ORGFILE
