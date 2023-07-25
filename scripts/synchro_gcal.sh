35fd20c3313805916ad16048718452ddf17ef9f4
CHARLES-HENRI PRAT <charprat@yahoo.fr>
2023-07-25 09:02:49 +0200

    [calendars] replace awk script by ica2orgpy

--- scripts/synchro_gcal.sh
+++ scripts/synchro_gcal.sh
@@ -2,10 +2,15 @@
 
 # customize these
 WGET=/opt/homebrew/bin/wget
-ICS2ORG=~/.emacs.d/scripts/ical2org.awk
+ICS2ORG=ical2orgpy
 ICSFILE=~/.emacs.d/bouboulinos.ics
 ORGFILE=~/.emacs.d/bouboulinos.org
 # no customization needed below
 
+export CALENDAR="perso"
 $WGET -O $ICSFILE $GCAL_PRIVATE_ADDRESS
-$ICS2ORG < $ICSFILE > $ORGFILE
+$ICS2ORG  $ICSFILE  $ORGFILE
+
+export CALENDAR="QM"
+$WGET -O $ICSFILE $QM_CALENDAR
+$ICS2ORG  $ICSFILE  $ORGFILE
