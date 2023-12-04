30a26d2c7594620d46461165788d2e436d5464ab
CHARLES-HENRI PRAT <charprat@yahoo.fr>
2023-12-04 12:03:29 +0100

    workaround for misalignement in sql prompt

--- init.el
+++ init.el
@@ -350,3 +350,15 @@ apps are not started from a shell."
                   (calendar-absolute-from-gregorian (list month day year)))))
         'font-lock-face 'calendar-iso-week-face))
 
+;; workaround for postgresql misalignement
+;; https://www.emacswiki.org/emacs/SqlMode#h5o-5
+(add-hook 'sql-login-hook 'my-sql-login-hook)
+(defun my-sql-login-hook ()
+  "Custom SQL log-in behaviours. See `sql-login-hook'."
+  ;; n.b. If you are looking for a response and need to parse the
+  ;; response, use `sql-redirect-value' instead of `comint-send-string'.
+  (when (eq sql-product 'postgres)
+    (let ((proc (get-buffer-process (current-buffer))))
+      ;; Output each query before executing it. (n.b. this also avoids
+      ;; the psql prompt breaking the alignment of query results.)
+      (comint-send-string proc "\\set ECHO queries\n"))))
