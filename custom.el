1d41cd122d3c30333c5f30abf770eba9be3f276d
Charles-Henri Prat <charprat@yahoo.fr>
2024-05-26 23:50:53 +0200

    add custom.el

---
+++ custom.el
@@ -0,0 +1,37 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(completion-category-overrides '((file (styles partial-completion))))
+ '(completion-styles '(orderless basic))
+ '(corfu-auto t)
+ '(corfu-auto-prefix 2)
+ '(corfu-cycle t)
+ '(evil-respect-visual-line-mode t)
+ '(evil-undo-system 'undo-redo)
+ '(evil-want-C-h-delete t)
+ '(evil-want-C-i-jump nil)
+ '(evil-want-integration t)
+ '(global-auto-revert-mode t)
+ '(load-prefer-newer t)
+ '(marginalia-annotators
+   '(marginalia-annotators-heavy marginalia-annotators-light nil) t)
+ '(mouse-wheel-progressive-speed nil)
+ '(package-archive-priorities
+   '(("gnu" . 99)
+     ("nongnu" . 80)
+     ("stable" . 70)
+     ("melpa" . 0)))
+ '(package-selected-packages
+   '(conda code-cells dashboard powerline-evil org-ai yaml-mode org-alert alert evil-nerd-commenter evil-collection evil jupyter flycheck projectile magit org-bullets dockerfile-mode poetry lsp-ui lsp-mode which-key exec-path-from-shell vertico orderless marginalia embark-consult embark corfu-terminal corfu consult cape))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(vertico-cycle t)
+ '(visible-bell t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
