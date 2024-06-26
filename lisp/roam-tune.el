366e3e2b7303101464aeed969006d8d2af456bad
Charles-Henri Prat <charprat@yahoo.fr>
2024-06-26 21:06:48 +0200

    use sqlite builtin

--- lisp/roam-tune.el
+++ lisp/roam-tune.el
@@ -11,6 +11,7 @@
   :custom
   (org-roam-directory "~/org-roam")
   (org-roam-completion-everywhere t)
+  (org-roam-database-connector 'sqlite-builtin)
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n i" . org-roam-node-insert)
