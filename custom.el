d4e0645cc44d77873453cd34924b4fd4f721c333
Charles-Henri Prat <charprat@yahoo.fr>
2024-06-26 22:16:12 +0200

    update custom.el

--- custom.el
+++ custom.el
@@ -8,23 +8,26 @@
  '(corfu-auto t)
  '(corfu-auto-prefix 2)
  '(corfu-cycle t)
+ '(custom-safe-themes
+   '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
  '(evil-respect-visual-line-mode t)
  '(evil-undo-system 'undo-redo)
  '(evil-want-C-h-delete t)
  '(evil-want-C-i-jump nil)
  '(evil-want-integration t)
  '(global-auto-revert-mode t)
- '(load-prefer-newer t)
+ '(load-prefer-newer t t)
  '(marginalia-annotators
    '(marginalia-annotators-heavy marginalia-annotators-light nil) t)
  '(mouse-wheel-progressive-speed nil)
+ '(org-roam-node-display-template #("${title:*} ${tags:10}" 11 21 (face org-tag)))
  '(package-archive-priorities
    '(("gnu" . 99)
      ("nongnu" . 80)
      ("stable" . 70)
      ("melpa" . 0)))
  '(package-selected-packages
-   '(conda code-cells dashboard powerline-evil org-ai yaml-mode org-alert alert evil-nerd-commenter evil-collection evil jupyter flycheck projectile magit org-bullets dockerfile-mode poetry lsp-ui lsp-mode which-key exec-path-from-shell vertico orderless marginalia embark-consult embark corfu-terminal corfu consult cape))
+   '(conda org-roam emacsql-sqlite-builtin blacken zenburn-theme code-cells dashboard powerline-evil org-ai yaml-mode org-alert alert evil-nerd-commenter evil-collection evil jupyter flycheck projectile magit org-bullets dockerfile-mode poetry lsp-ui lsp-mode which-key exec-path-from-shell vertico orderless marginalia embark-consult embark corfu-terminal corfu consult cape))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
  '(vertico-cycle t)
